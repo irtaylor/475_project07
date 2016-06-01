14214e2e14fc02ab9ef5e602982f1299974df23c
irtaylor <taylori@onid.oregonstate.edu>
2016-06-01 18:24:12 -0400

    Initial Commit

---
+++ 475_project07/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  475_project07
+//
+//  Created by Ian Taylor on 6/1/2016.
+//  Copyright © 2016 Ian Taylor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
