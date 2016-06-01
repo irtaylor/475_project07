993bd93230e0a096c90c13bf26814a2db1c77c1c
irtaylor <taylori@onid.oregonstate.edu>
2016-06-01 18:27:44 -0400

    initial commit

--- 475_project07/main.cpp
+++ 475_project07/main.cpp
@@ -7,8 +7,13 @@
 //
 
 #include <iostream>
+#include <OpenCL/OpenCL.h>
+#include <OpenGL/OpenGL.h>
 
 int main(int argc, const char * argv[]) {
+    
+    
+    
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
